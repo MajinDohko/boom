0a7aa8d0e2a33032d36a2e323c463ce60aa63262
MajinDohko <agarridoluengo@gmail.com>
2024-11-26 23:19:19 +0100

    cambios en js y css

--- js/script.js
+++ js/script.js
@@ -8,21 +8,20 @@ const botonReiniciar = document.getElementById('restart');
 let numeroJugador = document.getElementById('userInput');
 const arrayContador = [5, 4, 3, 2, 1, 0];
 console.log(numeroJugador);
-//localStorage.getItem('numeroOrdenador');
 
 function tiempoJuego () {
         setTimeout(() => {
             let numeroOrdenador = Math.floor(Math.random()*3 + 1);
             let valorJugada = numeroJugador.value;
             if (valorJugada == numeroOrdenador) {
-             resultado.textContent = `¡Has ganado, tu resultado ${valorJugada} es igual al número ${numeroOrdenador} !`;
+             resultado.innerHTML = `<h2 class="green">ENHORABUENA, HAS SALVADO EL MUNDO</h2>
+             <p>¡Has ganado, tu resultado ${valorJugada} es igual al número ${numeroOrdenador} !</p>`;
             } else {
-                resultado.textContent = `Perdiste, tu resultado ${valorJugada} es diferente al número ${numeroOrdenador} !`;
+                resultado.textContent = `¡Perdiste, tu resultado ${valorJugada} es diferente al número ${numeroOrdenador} !`;
             }
         }, 5000);
     }
     tiempoJuego();
-
 
 botonReiniciar.addEventListener('click', ()=>{
     resultado.textContent = "";
@@ -30,25 +29,7 @@ botonReiniciar.addEventListener('click', ()=>{
     tiempoJuego();
 })
 
-setInterval(cuentaAtras(), 1000);
 
 function cuentaAtras() {
     
 }
-    /*arrayContador.forEach(numero => {
-        setInterval(() => {
-            numero = arrayContador[numero];
-            let imprimirNumero = document.createElement('p');
-            imprimirNumero.innerText = numero;
-            contador.appendChild(imprimirNumero);
-            console.log(imprimirNumero);
-        }, 1000);
-    });*/
-    /*for (let i = 0; i < arrayContador.length; i++) {
-        const numero = arrayContador[i];
-        let imprimirNumero = document.createElement('p');
-            imprimirNumero.innerText = numero;
-            contador.appendChild(imprimirNumero);
-            console.log(imprimirNumero);
-    }
-}*/
